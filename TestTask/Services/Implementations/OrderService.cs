4bf384581cde466b3d7f51a668c6cb11763fbe72
Pavel Bareyko <pavel.bareyko8@gmail.com>
2024-04-05 18:46:21 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext db;
+
+    public OrderService(ApplicationDbContext db)
+    {
+        this.db = db;
+    }
+
+    public Task<Order> GetOrder()
+    {
+        var orderWithLargestOrderPrice = db.Orders.OrderByDescending(o => o.Price).FirstOrDefault()!;
+        return Task.FromResult(orderWithLargestOrderPrice);
+    }
+
+    public Task<List<Order>> GetOrders()
+    {
+        var ordersInWhichTheQuantityOfGoodsIsMoreThanTen = db.Orders.Where(o => o.Quantity > 10);
+        return Task.FromResult(ordersInWhichTheQuantityOfGoodsIsMoreThanTen.ToList());
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,27 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext db;
+
+    public UserService(ApplicationDbContext db)
+    {
+        this.db = db;
+    }
+
+    public Task<User> GetUser()
+    {
+        var userWithMaxOrders = db.Users.OrderByDescending(u => u.Orders.Count).FirstOrDefault()!;
+        return Task.FromResult(userWithMaxOrders);
+    }
+
+    public Task<List<User>> GetUsers()
+    {
+        var usersWithInactiveStatus = db.Users.Where(u => u.Status == Enums.UserStatus.Inactive);
+        return Task.FromResult(usersWithInactiveStatus.ToList());
+    }
+}
